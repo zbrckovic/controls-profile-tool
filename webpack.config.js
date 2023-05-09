7517d91ccc28cdd452879a6cb97fccc64ffedc12
Željko Brcković <Zeljko.Brckovic1@infobip.com>
2023-05-09 20:41:23 +0200

    Added some base project config files

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = ({ mode }) => ({
+  mode: mode || 'development',
+  entry: './src/index.jsx',
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ]
+})
